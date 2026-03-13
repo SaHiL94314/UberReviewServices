2ef7c7686d9943fc2b659a9fd64433b05bfade44
SaHil94314 <sahilsinha.ayush@gmail.com>
2026-03-13 17:14:47 +0530

    added booking association to review

---
+++ src/main/java/com/example/UberReviewService/Models/Booking.java
@@ -0,0 +1,29 @@
+package com.example.UberReviewService.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel{
+
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    private Review review; // there is one to one mapping between booking and review
+
+    @Enumerated(value= EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+}
--- src/main/java/com/example/UberReviewService/Services/ReviewServices.java
+++ src/main/java/com/example/UberReviewService/Services/ReviewServices.java
@@ -1,6 +1,8 @@
 package com.example.UberReviewService.Services;
 
+import com.example.UberReviewService.Models.Booking;
 import com.example.UberReviewService.Models.Review;
+import com.example.UberReviewService.Repositories.BookingRepository;
 import com.example.UberReviewService.Repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
@@ -11,19 +13,29 @@ import java.util.List;
 @Service
 public class ReviewServices implements CommandLineRunner {
     private ReviewRepository reviewRepository;
-
-    public ReviewServices(ReviewRepository reviewRepository){
+    private BookingRepository bookingRepository;
+    public ReviewServices(ReviewRepository reviewRepository,BookingRepository bookingRepository){
         this.reviewRepository=reviewRepository;
+        this.bookingRepository=bookingRepository;
     }
     @Override
     public void run(String... args) throws Exception {
          System.out.println("****************");
-//         Review r= Review.builder()
-//                 .content("amazing ride")
-//                 .rating(5.0)
-//                 .build();
+         Review r= Review.builder()
+                 .content("amazing ride")
+                 .rating(5.0)
+                 .build();
 //         System.out.println(r);
-//         reviewRepository.save(r);
+        Booking b=Booking.builder()
+                        .startTime(new Date())
+                        .endTime(new Date())
+                        .review(r)
+                        .build();
+
+//        reviewRepository.save(r);
+//        bookingRepository.save(b);
+
+        bookingRepository.deleteById(3L);
 //
 //         List<Review> reviews=reviewRepository.findAll();
 //         for(Review re:reviews){
